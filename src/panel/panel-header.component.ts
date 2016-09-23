d850fdf9a1f2b83d3126f61f867b97bf4bd2b70c
Jonas Möller <moeller-jonas@web.de>
2016-09-23 09:22:42 +0200

    Add pin to panel-header

--- src/panel/panel-header.component.ts
+++ src/panel/panel-header.component.ts
@@ -60,29 +60,44 @@ import { NodeInterface } from '../node/treenode.interface';
 			z-index: 100;
 		}
 
-		.ee-icon span {
+		.ee-icon>span {
 			font-weight: bold;
-			padding: 5px 12px 5px 12px;
 			border-radius: 4px;
 			cursor: pointer;
 			border: 1px solid transparent;
+			display: block;
+			width: 34px;
+			height: 32px;
+			text-align: center;
+			line-height: 28px;
 		}
 
-		.ee-icon span:hover {
+		.ee-icon>span:hover {
 			border: 1px solid #aaa;
+		}
+
+		.ee-icon span.pinned .glyphicon {
+			transform: rotate(45deg) scale(0.9);
 		}
 	`],
 	template: `
 		<div class="ee-panel-header" [closeEmitter]="closeEmitter" [node]="node" dragStart="'panel'">
 			<div class="ee-panel-header-heading">
 				<span [hidden]="changeName" (click)="toggleName()">{{node?.name}}</span>
-				<form [hidden]="!changeName" (ngSubmit)="toggleName()">
+				<form [hidden]="!changeName" (ngSubmit)="submit()">
 					<input type="text" [(ngModel)]="node.name" [ngModelOptions]="{standalone: true}">
 					<input type="submit" value="Ok">
 				</form>
 			</div>
 			<div class="ee-panel-header-space"></div>
-			<!--<div class="ee-icon flex"><span (click)="minimize()">_</span></div>-->
+			<div class="ee-icon">
+				<span class="glyphicon glyphicon-save" *ngIf="!saved" (click)="save()"></span>
+			</div>
+			<div class="ee-icon">
+				<span [ngClass]="{'pinned': pinned}" (click)="pin()">
+					<span class="glyphicon glyphicon-pushpin"></span>
+				</span>
+			</div>
 			<div class="ee-icon"><span (click)="close()">x</span></div>
 		</div>
 	`
@@ -90,9 +105,13 @@ import { NodeInterface } from '../node/treenode.interface';
 
 export class PanelHeaderComponent {
 	@Input() node: NodeInterface.TreeNode;
+
 	@Output("close") closeEmitter: EventEmitter<void> = new EventEmitter<void>();
+	@Output("pin") pinEmitter: EventEmitter<boolean> = new EventEmitter<boolean>();
 
 	changeName: boolean = false;
+	pinned: boolean = false;
+	saved: boolean = true;
 
 	constructor() { }
 
@@ -100,7 +119,21 @@ export class PanelHeaderComponent {
 		this.closeEmitter.emit();
 	}
 
+	submit() {
+		this.toggleName();
+		this.saved = false;
+	}
+
 	toggleName() {
 		this.changeName = !this.changeName;
+	}
+
+	save() {
+		this.saved = true;
+	}
+
+	pin() {
+		this.pinned = !this.pinned;
+		this.pinEmitter.emit(this.pinned);
 	}
 }
